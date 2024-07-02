8b232e589e4fc079401fedbb0de471ea00cd371f
Francesca3003 <francescapischedda30@gmail.com>
2024-07-02 14:31:23 +0200

    updated employeeDashboard

--- Gestionale-Proyect/src/components/EmployeeDashboard.jsx
+++ Gestionale-Proyect/src/components/EmployeeDashboard.jsx
@@ -54,7 +54,62 @@ const EmployeeDashboard = () => {
             </div>
           )}
         </div>
-        {/* Altri contenuti della dashboard */}
+        <div className="scrolling-container">
+          <div className="day-card free">
+            <span>Monday 01</span>
+            <span>Free</span>
+          </div>
+          <div className="day-card occupied">
+            <span>Tuesday 02</span>
+            <span>Occupied</span>
+          </div>
+          <div className="day-card free">
+            <span>Wednesday 03</span>
+            <span>Free</span>
+          </div>
+          <div className="day-card occupied">
+            <span>Thursday 04</span>
+            <span>Occupied</span>
+          </div>
+          <div className="day-card free">
+            <span>Friday 05</span>
+            <span>Free</span>
+          </div>
+        </div>
+        <div className="card">
+          <h3>On-Duty Workers</h3>
+          <ul>
+            <li>Jimmy Saavedra - 7:00 AM</li>
+            <li>Francesca - 8:00 AM</li>
+            <li>Giorgio - 7:00 AM</li>
+            <li>Ricardo -</li>
+          </ul>
+        </div>
+        <div className="card">
+          <div className="card-header">
+            <h3>Notifications</h3>
+            <i className="fas fa-bell"></i>
+          </div>
+          <p>Don't forget we need to work as a team</p>
+        </div>
+      </div>
+      <div className="footer">
+        <a href="#">
+          <i className="fas fa-home"></i>
+          <span>Home</span>
+        </a>
+        <a href="#">
+          <i className=""></i>
+          <span>Dashboard</span>
+        </a>
+        <a href="#">
+          <i className="fas fa-user"></i>
+          <span>Profile</span>
+        </a>
+        <a href="/settings">
+          <i className="fas fa-cog"></i>
+          <span>Settings</span>
+        </a>
       </div>
     </div>
   );
