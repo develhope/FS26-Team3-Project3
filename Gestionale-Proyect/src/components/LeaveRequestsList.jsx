45553d1899ff88a52a028cc0a0e7f4653e1d5008
Giorgio <147600523+Quintavalle@users.noreply.github.com>
2024-07-17 06:21:31 +0200

    commit new changes

--- Gestionale-Proyect/src/components/LeaveRequestsList.jsx
+++ Gestionale-Proyect/src/components/LeaveRequestsList.jsx
@@ -1,6 +1,5 @@
-
 import React from 'react';
-import './LeaveRequestsList.css'; // Assicurati di importare il file CSS
+import './LeaveRequestsList.css';
 
 const LeaveRequestsList = ({ requests, onApprove, onDeny }) => {
   return (
@@ -9,10 +8,12 @@ const LeaveRequestsList = ({ requests, onApprove, onDeny }) => {
       <ul>
         {requests.map((request, index) => (
           <li key={index} className="request-item">
-            <p><strong>Leave Type:</strong> {request.leaveType}</p>
-            <p><strong>Period:</strong> from {request.startDate} to {request.endDate}</p>
-            <p><strong>Status:</strong> {request.status}</p>
-            <p><strong>Reason:</strong> {request.reason}</p>
+            <div className="request-details">
+              <p><strong>Leave Type:</strong> {request.leaveType}</p>
+              <p><strong>Period:</strong> from {request.startDate} to {request.endDate}</p>
+              <p><strong>Reason:</strong> {request.reason}</p>
+              <p><strong>Status:</strong> {request.status}</p>
+            </div>
             <div className="buttons">
               <button className="approve-button" onClick={() => onApprove(index)}>Approve</button>
               <button className="deny-button" onClick={() => onDeny(index)}>Deny</button>
