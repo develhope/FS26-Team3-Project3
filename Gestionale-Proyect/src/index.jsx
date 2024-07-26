a60c47d755daac16d68f790b8920f1ee4e5112b6
Giorgio <147600523+Quintavalle@users.noreply.github.com>
2024-07-26 13:02:24 +0200

    commit

--- Gestionale-Proyect/src/index.jsx
+++ Gestionale-Proyect/src/index.jsx
@@ -1,7 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import './index.css';
 import "./styles/main.css"; 
 import App from "./App";
+import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
@@ -9,3 +11,5 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById("root")
 );
+
+reportWebVitals();
