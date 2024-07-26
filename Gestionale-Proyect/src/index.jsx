9cd2f660a1218c6dc00fe9cbfe5837c422bd5060
Giorgio <147600523+Quintavalle@users.noreply.github.com>
2024-07-26 14:19:23 +0200

    commit

--- Gestionale-Proyect/src/index.jsx
+++ Gestionale-Proyect/src/index.jsx
@@ -1,33 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css'; // Importa il CSS principale
-import App from './App'; // Importa il componente principale
-import reportWebVitals from './reportWebVitals';
+import './index.css'; 
+import App from './App'; 
 
-// Funzione per resettare i dati di localStorage
-const resetLocalStorage = () => {
-  console.log('Resetting localStorage for development environment');
-  localStorage.clear();
-  const users = [
-    { firstName: 'Riccardo', lastName: 'Cuomo', email: 'cuomo.riccardo@gmail.com', password: 'qwerty', id: 'RC', role: 'user' },
-    { firstName: 'Francesca', lastName: 'Pischedda', email: 'francesca.pischedda@gmail.com', password: 'asdfgh', id: 'FP', role: 'supervisor' },
-    { firstName: 'Mario', lastName: 'Rossi', email: 'mario.rossi@gmail.com', password: 'zxcvbn', id: 'MR', role: 'user' }
-  ];
-  localStorage.setItem('users', JSON.stringify(users));
-  console.log('localStorage reset with users:', users);
-};
-
-// Resetta i dati di localStorage se l'app è in modalità sviluppo
-if (process.env.NODE_ENV === 'development') {
-  resetLocalStorage();
-}
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
-
-reportWebVitals();
-
